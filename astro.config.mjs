133db3c27c6f054d494186f02ae76d577d2ed464
Bartosz <bartosz.janiuk@tsh.io>
2024-07-21 21:16:12 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321/",
-  integrations: [react(), tailwind()],
-  output: "server"
-  // adapter: netlify(),
+	site: "http://localhost:4321/",
+	integrations: [react(), tailwind()],
+	output: "server",
+	adapter: netlify(),
 });
